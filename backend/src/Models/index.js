eba3ffd2056174f8996dbe73c436ae1716cae1b6
rabetokotany41 <rabetokotanyenzo@gmail.com>
2026-08-20 14:39:38 +0300

    Add models index

---
+++ backend/src/Models/index.js
@@ -0,0 +1,23 @@
+import User from "./User.js";
+import AcademicYear from "./AcademicYear.js";
+import Semester from "./Semester.js";
+import Group from "./Group.js";
+import Salle from "./Salle.js";
+import Subject from "./Subject.js";
+import Enrollment from "./Enrollment.js";
+import Indisponibilite from "./Indisponibilite.js";
+import Cours from "./Cours.js";
+import Alerte from "./Alerte.js";
+
+export {
+  User,
+  AcademicYear,
+  Semester,
+  Group,
+  Salle,
+  Subject,
+  Enrollment,
+  Indisponibilite,
+  Cours,
+  Alerte,
+};
